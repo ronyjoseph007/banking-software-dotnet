1f77187fbed8b267d21b68c44258fd479dab3090
ronyjoseph007 <ronyjoseph1998@gmail.com>
2021-08-12 12:46:36 -0700

    balance and withdraw features added

--- bamking application/Form2.Designer.cs
+++ bamking application/Form2.Designer.cs
@@ -30,8 +30,8 @@ namespace bamking_application
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -39,22 +39,22 @@ namespace bamking_application
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button7 = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button8 = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -67,17 +67,6 @@ namespace bamking_application
             this.panel1.Size = new System.Drawing.Size(1160, 92);
             this.panel1.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Consolas", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(14, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 47);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "SBI BANK";
-            // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
@@ -89,6 +78,17 @@ namespace bamking_application
             this.linkLabel1.TabIndex = 1;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Logout";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SBI BANK";
             // 
             // button1
             // 
@@ -180,6 +180,39 @@ namespace bamking_application
             this.panel2.Size = new System.Drawing.Size(1098, 399);
             this.panel2.TabIndex = 7;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(18, 23);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1058, 119);
+            this.panel3.TabIndex = 4;
+            this.panel3.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(424, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label2.Location = new System.Drawing.Point(283, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Balance";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // panel4
             // 
             this.panel4.Controls.Add(this.button7);
@@ -191,26 +224,6 @@ namespace bamking_application
             this.panel4.Size = new System.Drawing.Size(1058, 325);
             this.panel4.TabIndex = 2;
             this.panel4.Visible = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.White;
-            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.SkyBlue;
-            this.label4.Location = new System.Drawing.Point(287, 46);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 19);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Enter Amount";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(427, 47);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 20);
-            this.textBox1.TabIndex = 1;
             // 
             // button7
             // 
@@ -231,31 +244,11 @@ namespace bamking_application
             this.panel5.Controls.Add(this.button8);
             this.panel5.Controls.Add(this.textBox2);
             this.panel5.Controls.Add(this.label5);
-            this.panel5.Location = new System.Drawing.Point(37, 143);
+            this.panel5.Location = new System.Drawing.Point(43, 146);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(994, 153);
             this.panel5.TabIndex = 3;
             this.panel5.Visible = false;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.SkyBlue;
-            this.label5.Location = new System.Drawing.Point(246, 47);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(107, 19);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "PAN Number";
-            this.label5.Click += new System.EventHandler(this.label5_Click_1);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(389, 48);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(209, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button8
             // 
@@ -271,38 +264,45 @@ namespace bamking_application
             this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
-            // panel3
+            // textBox2
             // 
-            this.panel3.Controls.Add(this.label3);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(21, 23);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1058, 119);
-            this.panel3.TabIndex = 4;
-            this.panel3.Visible = false;
+            this.textBox2.Location = new System.Drawing.Point(389, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // label2
+            // label5
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.SkyBlue;
-            this.label2.Location = new System.Drawing.Point(283, 34);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 19);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Balance";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label5.Location = new System.Drawing.Point(246, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "PIN Number";
+            this.label5.Click += new System.EventHandler(this.label5_Click_1);
             // 
-            // label3
+            // textBox1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(424, 35);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 18);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "label3";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.textBox1.Location = new System.Drawing.Point(427, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label4.Location = new System.Drawing.Point(287, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Enter Amount";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form2
             // 
@@ -324,12 +324,12 @@ namespace bamking_application
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- bamking application/Form2.cs
+++ bamking application/Form2.cs
@@ -14,6 +14,7 @@ namespace bamking_application
     public partial class Form2 : Form
     {
         String accno;
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-GHBONFT\SQLEXPRESS;Database=sbibank;Integrated Security=true");
         public Form2(string acc)
         {
             InitializeComponent();
@@ -44,20 +45,69 @@ namespace bamking_application
         {
 
         }
-
+        //confirm
         private void button8_Click(object sender, EventArgs e)
         {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from accdetails where accnum='" + accno + "' and pin_num='" + textBox2.Text + "'",con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                con.Open();
+                SqlCommand cmd1 = new SqlCommand("select * from accdetails where accnum='"+accno+"'",con);
+                SqlDataReader dr1 = cmd1.ExecuteReader();
+                if (dr1.Read())
+                {
+                    int balance = int.Parse(dr1.GetValue(3).ToString());
+                    int withdraw = int.Parse(textBox1.Text);
+                    int updat_bal = balance - withdraw;
+                    con.Close();
+                    con.Open();
+                    SqlCommand cmd2 = new SqlCommand("update accdetails set balance='"+updat_bal+"' where accnum='"+accno+"'",con);
+                    cmd2.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("amount debited successfully");
 
+                }
+
+            }
         }
-
+        //continue
         private void button7_Click(object sender, EventArgs e)
         {
-            panel5.Visible = true;
-        }
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from accdetails where accnum='"+accno+"'",con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                int bal = int.Parse(dr.GetValue(3).ToString());
+                int withdraw = int.Parse(textBox1.Text);
+                if (withdraw < bal)
+                {
+                    panel5.Visible = true;
+                }
+                else
+                {
+                    MessageBox.Show("Insufficient Balance");
+                }
+                con.Close();
+            }
 
+        }
+        //balance
         private void button1_Click(object sender, EventArgs e)
         {
             panel3.Visible = true;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from accdetails where accnum='"+accno+"'",con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                label3.Text = dr.GetValue(3).ToString();
+            }
+            con.Close();
+
         }
 
         private void label2_Click(object sender, EventArgs e)
