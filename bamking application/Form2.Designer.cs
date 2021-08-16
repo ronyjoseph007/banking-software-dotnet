6a405d3f382b00cad5311fd92d4aed56299054bf
ronyjoseph007 <ronyjoseph1998@gmail.com>
2021-08-15 20:29:46 -0700

    deposit section completed

--- bamking application/Form2.Designer.cs
+++ bamking application/Form2.Designer.cs
@@ -50,11 +50,31 @@ namespace bamking_application
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -130,6 +150,7 @@ namespace bamking_application
             this.button3.TabIndex = 3;
             this.button3.Text = "Deposit";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -173,18 +194,19 @@ namespace bamking_application
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel6);
             this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Location = new System.Drawing.Point(17, 222);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1098, 399);
+            this.panel2.Size = new System.Drawing.Size(1098, 600);
             this.panel2.TabIndex = 7;
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(18, 23);
+            this.panel3.Location = new System.Drawing.Point(21, 23);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1058, 119);
             this.panel3.TabIndex = 4;
@@ -221,7 +243,7 @@ namespace bamking_application
             this.panel4.Controls.Add(this.label4);
             this.panel4.Location = new System.Drawing.Point(21, 23);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1058, 325);
+            this.panel4.Size = new System.Drawing.Size(1058, 559);
             this.panel4.TabIndex = 2;
             this.panel4.Visible = false;
             // 
@@ -244,7 +266,7 @@ namespace bamking_application
             this.panel5.Controls.Add(this.button8);
             this.panel5.Controls.Add(this.textBox2);
             this.panel5.Controls.Add(this.label5);
-            this.panel5.Location = new System.Drawing.Point(43, 146);
+            this.panel5.Location = new System.Drawing.Point(19, 138);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(994, 153);
             this.panel5.TabIndex = 3;
@@ -277,7 +299,7 @@ namespace bamking_application
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.SkyBlue;
-            this.label5.Location = new System.Drawing.Point(246, 47);
+            this.label5.Location = new System.Drawing.Point(268, 47);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(101, 19);
             this.label5.TabIndex = 0;
@@ -304,12 +326,199 @@ namespace bamking_application
             this.label4.Text = "Enter Amount";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Controls.Add(this.button9);
+            this.panel6.Controls.Add(this.textBox3);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Location = new System.Drawing.Point(21, 23);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(1058, 559);
+            this.panel6.TabIndex = 3;
+            this.panel6.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label6.Location = new System.Drawing.Point(300, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Enter amount";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(417, 47);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 20);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.Location = new System.Drawing.Point(693, 41);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(92, 34);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Continue";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.button10);
+            this.panel7.Controls.Add(this.label12);
+            this.panel7.Controls.Add(this.label11);
+            this.panel7.Controls.Add(this.label10);
+            this.panel7.Controls.Add(this.textBox9);
+            this.panel7.Controls.Add(this.textBox8);
+            this.panel7.Controls.Add(this.textBox7);
+            this.panel7.Controls.Add(this.textBox6);
+            this.panel7.Controls.Add(this.textBox5);
+            this.panel7.Controls.Add(this.label9);
+            this.panel7.Controls.Add(this.label8);
+            this.panel7.Controls.Add(this.textBox4);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Location = new System.Drawing.Point(161, 125);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(791, 341);
+            this.panel7.TabIndex = 3;
+            this.panel7.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(104, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 16);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "200 *";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(217, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(104, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 16);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "500 *";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(104, 183);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 16);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "2000 *";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(217, 134);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(217, 183);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(463, 83);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(463, 137);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(463, 185);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 8;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(379, 83);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(17, 17);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "=";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(379, 139);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 17);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "=";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(379, 188);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 17);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "= ";
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.Location = new System.Drawing.Point(476, 227);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(87, 34);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Confirm";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1155, 660);
+            this.ClientSize = new System.Drawing.Size(1155, 834);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -330,6 +539,10 @@ namespace bamking_application
             this.panel4.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -357,5 +570,23 @@ namespace bamking_application
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button10;
     }
 }
--- bamking application/Form2.cs
+++ bamking application/Form2.cs
@@ -114,9 +114,10 @@ namespace bamking_application
         {
 
         }
-
+        //withdraw
         private void button2_Click(object sender, EventArgs e)
         {
+            panel6.Visible = false;
             panel4.Visible = true;
             panel3.Visible = false;
         }
@@ -124,6 +125,86 @@ namespace bamking_application
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+        //deposit
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+            panel4.Visible = false;
+            panel6.Visible = true;
+        }
+        //continue_deposit
+        private void button9_Click(object sender, EventArgs e)
+        {
+            panel7.Visible = true;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox4.Text);
+            int b = 200 * a;
+            textBox7.Text = b.ToString();
+        }
+
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox5.Text);
+            int b = 500 * a;
+            textBox8.Text = b.ToString();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox6.Text);
+            int b = 2000 * a;
+            textBox9.Text = b.ToString();
+        }
+        //confirm_deposit
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int aa = int.Parse(textBox7.Text);
+            int bb = int.Parse(textBox8.Text);
+            int cc = int.Parse(textBox9.Text);
+            int deposit = int.Parse(textBox3.Text);
+            int dd = aa + bb + cc;
+            if (dd == deposit)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from  accdetails where accnum='"+accno+"'",con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    int bal = int.Parse(dr.GetValue(3).ToString());
+                    int upbal = bal + deposit;
+                    con.Close();
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand("update accdetails set balance='"+upbal+"' where accnum='"+accno+"'",con);
+                    cmd1.ExecuteNonQuery();
+                    MessageBox.Show("Amount deposited successfully");
+                    con.Close();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ceck the denominator");
+            }
         }
     }
 }
